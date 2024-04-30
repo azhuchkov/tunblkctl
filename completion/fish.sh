ee6494e60433a19c6e4fdbb8dcd19b3cf84a547b
Andrey Zhuchkov <zhuchkovas@gmail.com>
2024-04-30 14:49:13 +0400

    #3 Support Fish shell

---
+++ completion/fish.sh
@@ -0,0 +1,21 @@
+# Define subcommands list
+set -l commands list status connect disconnect quit
+
+# Disable filesystem completions
+complete -c tunblkctl --no-files
+
+# Complete commands
+complete -c tunblkctl --condition "not __fish_seen_subcommand_from $commands" \
+  -a "$commands"
+
+# Complete VPN names
+complete -c tunblkctl --condition "__fish_seen_subcommand_from connect disconnect" \
+    -a "(tunblkctl list)"
+
+# Complete strip option
+complete -c tunblkctl --condition "__fish_seen_subcommand_from status" \
+    -l strip -d 'Strip output formatting (raw mode)'
+
+# Complete no-strip option
+complete -c tunblkctl --condition "__fish_seen_subcommand_from status" \
+    -l no-strip -d 'Do not strip output formatting (human mode)'
